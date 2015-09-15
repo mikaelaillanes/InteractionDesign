4140d843530d370692cd492c1054d6b2bdbd9162
mariliab <marilia.bognandi@gmail.com>
2015-09-15 12:49:25 +0200

    hej

--- js/index.js
+++ js/index.js
@@ -1,11 +1,18 @@
 /*index.js*/
+var inputText = "Stockholm";
+
 $(function(){
 
 
     $('#knapp1').click(function(){
       jQuery.mobile.changePage('#pagetwo');
       console.log("hej");
+      var inputTexten = document.getElementById("#inputThingToDo").value;
+      console.log(inputTexten);
     });
+
+
+
 
 
 
